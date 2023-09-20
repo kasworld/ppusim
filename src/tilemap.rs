f974083cfeaec977030f8f24acb1811ca1022123
SeukWon Kang <kasworld@gmail.com>
2023-09-20 11:12:31 +0900

    wip tilemap render

--- src/tilemap.rs
+++ src/tilemap.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 use crate::{palette, render_dst, tile, tile_vec, tilemap_buffer};
 
 // sprite or background plane
@@ -26,6 +28,32 @@ impl TileMap {
             enable: false,
         }
     }
+    pub fn calc_render_vars(
+        self,
+        dstw: usize,
+        dsth: usize,
+    ) -> (usize, usize, usize, usize, usize, usize) {
+        let (render_start_x, tile_start_x) = if self.pos.0 < 0 {
+            (0 as usize, -self.pos.0 as usize)
+        } else {
+            (self.pos.0 as usize, 0 as usize)
+        };
+        let (render_start_y, tile_start_y) = if self.pos.1 < 0 {
+            (0 as usize, -self.pos.1 as usize)
+        } else {
+            (self.pos.1 as usize, 0 as usize)
+        };
+        let render_width = cmp::min(self.wh.0 as usize - tile_start_x, dstw - render_start_x);
+        let render_height = cmp::min(self.wh.1 as usize - tile_start_y, dsth - render_start_y);
+        return (
+            render_start_x,
+            render_start_y,
+            tile_start_x,
+            tile_start_y,
+            render_width,
+            render_height,
+        );
+    }
     pub fn render(
         self,
         mut dst: &render_dst::RenderDst,
@@ -39,6 +67,15 @@ impl TileMap {
         let tmw = self.wh.0 as usize;
         let tmh = self.wh.1 as usize;
         let tilemapbuff = tilemapbuffer.get_buffer(self.tilemap_buffer_index as usize, tmw, tmh);
+
+        let (
+            render_start_x,
+            render_start_y,
+            tile_start_x,
+            tile_start_y,
+            render_width,
+            render_height,
+        ) = self.calc_render_vars(dst.w, dst.h);
 
         for tmy in 0..tmh {
             for tmx in 0..tmw {
