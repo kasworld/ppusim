93b9ce40f48c59bd1667430badb31c8df3a898f0
SeukWon Kang <kasworld@gmail.com>
2023-09-21 18:12:19 +0900

    tile def load from image

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ fn main() {
     let begin = Instant::now();
 
     let palette = palette::Palette::new_random();
-    let tile_def = tile_vec::TileVec::new_random();
+    // let tile_def = tile_vec::TileVec::new_random();
+    let mut tile_def = tile_vec::TileVec::load_from_file("tilesdef.bmp".to_string());
     let tile_map_def = tilemap_vec::TileMapVec::new_random(DSTW, DSTH);
     let tile_map_buffer = tilemap_buffer::TileMapBuffer::new_random();
     let mut rnd_dst = &mut image::RgbaImage::new(DSTW as u32, DSTH as u32);
--- src/tile_vec.rs
+++ src/tile_vec.rs
@@ -1,4 +1,7 @@
+use std::{fs::File, io::BufReader};
+
 use crate::tile;
+use image::{self, GenericImageView, Pixel};
 
 pub const UPPER_TILE_VEC_SIZE: usize = 256;
 
@@ -22,5 +25,41 @@ impl TileVec {
     }
     pub fn get_at(&self, hi: u8, index: usize) -> tile::Tile {
         self.0[hi as usize * LOWER_TILE_VEC_SIZE + index]
+    }
+
+    // file must bmp 256 color grayscale
+    // size TILE_WIDTH * LOWER_TILE_VEC_SIZE
+    // size TILE_HEIGHT * UPPER_TILE_VEC_SIZE
+    pub fn load_from_file(filename: String)->Self {
+        let mut rtn = Self(vec![tile::new_empty(); TILE_VEC_SIZE]);
+        let f = match File::open(filename) {
+            Ok(f) => f,
+            Err(err) => {
+                println!("open file {err}");
+                return rtn;
+            }
+        };
+        let img = match image::load(BufReader::new(f), image::ImageFormat::Bmp) {
+            Ok(v) => v,
+            Err(err) => {
+                println!("load image {err}");
+                return rtn;
+            }
+        };
+        let width = tile::TILE_WIDTH as usize * LOWER_TILE_VEC_SIZE as usize;
+        let height = tile::TILE_HEIGHT as usize * UPPER_TILE_VEC_SIZE as usize;
+        assert_eq!(width, img.width() as usize);
+        assert_eq!(height, img.width() as usize);
+        for y in 0..height {
+            let tly = y / tile::TILE_HEIGHT as usize;
+            let tly_d = y % tile::TILE_HEIGHT as usize;
+            for x in 0..width {
+                let tlx = x / tile::TILE_WIDTH as usize;
+                let tlx_d = x % tile::TILE_WIDTH as usize;
+                let px = img.get_pixel(x as u32, y as u32).to_luma().0[0];
+                rtn.0[tlx + tly * LOWER_TILE_VEC_SIZE as usize][tly_d][tlx_d] = px;
+            }
+        }
+        rtn
     }
 }
