4e88e4fbb4ff21546d3be8d6617f62b453e4f0be
SeukWon Kang <kasworld@gmail.com>
2023-09-22 13:35:38 +0900

    change tilevec file format

--- src/tile_vec.rs
+++ src/tile_vec.rs
@@ -31,6 +31,7 @@ impl TileVec {
     // file must bmp 256 color grayscale
     // size TILE_WIDTH * LOWER_TILE_VEC_SIZE
     // size TILE_HEIGHT * UPPER_TILE_VEC_SIZE
+    // 16 x 16 of ( 16 tile x 16 tile) image
     pub fn load_from_file(filename: String) -> Self {
         let mut rtn = Self(vec![tile::new_empty(); TILE_VEC_SIZE]);
         let f = match File::open(filename) {
@@ -51,14 +52,27 @@ impl TileVec {
         let height = tile::TILE_HEIGHT as usize * UPPER_TILE_VEC_SIZE as usize;
         assert_eq!(width, img.width() as usize);
         assert_eq!(height, img.width() as usize);
-        for y in 0..height {
-            let tly = y / tile::TILE_HEIGHT as usize;
-            let tly_d = y % tile::TILE_HEIGHT as usize;
-            for x in 0..width {
-                let tlx = x / tile::TILE_WIDTH as usize;
-                let tlx_d = x % tile::TILE_WIDTH as usize;
-                let px = img.get_pixel(x as u32, y as u32).to_luma().0[0];
-                rtn.0[tlx + tly * LOWER_TILE_VEC_SIZE as usize][tly_d][tlx_d] = px;
+
+        let sqrt_lower_tile_vec_size = 16; // sqrt(LOWER_TILE_VEC_SIZE)
+        let mut tile_index = 0;
+        for up_y in 0..sqrt_lower_tile_vec_size {
+            for up_x in 0..sqrt_lower_tile_vec_size {
+                for dn_y in 0..sqrt_lower_tile_vec_size {
+                    for dn_x in 0..sqrt_lower_tile_vec_size {
+                        for ty in 0..tile::TILE_HEIGHT {
+                            for tx in 0..tile::TILE_WIDTH {
+                                let x = (up_x * sqrt_lower_tile_vec_size + dn_x) * tile::TILE_WIDTH
+                                    + tx;
+                                let y = (up_y * sqrt_lower_tile_vec_size + dn_y)
+                                    * tile::TILE_HEIGHT
+                                    + ty;
+                                let px = img.get_pixel(x as u32, y as u32).to_luma().0[0];
+                                rtn.0[tile_index][ty][tx] = px;
+                            }
+                        }
+                        tile_index += 1;
+                    }
+                }
             }
         }
         rtn
@@ -69,17 +83,27 @@ impl TileVec {
             (LOWER_TILE_VEC_SIZE * (tile::TILE_WIDTH as usize)) as u32,
             (UPPER_TILE_VEC_SIZE * (tile::TILE_HEIGHT as usize)) as u32,
         );
-        for y in 0..UPPER_TILE_VEC_SIZE {
-            for x in 0..LOWER_TILE_VEC_SIZE {
-                for tx in 0..tile::TILE_WIDTH {
-                    let tl = self.0[x + y * LOWER_TILE_VEC_SIZE];
-                    for ty in 0..tile::TILE_HEIGHT {
-                        let px = tl[ty][tx];
-                        img.put_pixel(
-                            (x * (tile::TILE_WIDTH as usize) + tx) as u32,
-                            (y * (tile::TILE_HEIGHT as usize) + ty) as u32,
-                            image::Luma([px]),
-                        );
+
+        let sqrt_lower_tile_vec_size = 16; // sqrt(LOWER_TILE_VEC_SIZE)
+        let mut tile_index = 0;
+        for up_y in 0..sqrt_lower_tile_vec_size {
+            for up_x in 0..sqrt_lower_tile_vec_size {
+                for dn_y in 0..sqrt_lower_tile_vec_size {
+                    for dn_x in 0..sqrt_lower_tile_vec_size {
+                        for ty in 0..tile::TILE_HEIGHT {
+                            for tx in 0..tile::TILE_WIDTH {
+                                let x = (up_x * sqrt_lower_tile_vec_size + dn_x) * tile::TILE_WIDTH
+                                    + tx;
+                                let y = (up_y * sqrt_lower_tile_vec_size + dn_y)
+                                    * tile::TILE_HEIGHT
+                                    + ty;
+
+                                let tl = self.0[tile_index];
+                                let px = tl[ty][tx];
+                                img.put_pixel(x as u32, y as u32, image::Luma([px]));
+                            }
+                        }
+                        tile_index += 1;
                     }
                 }
             }
